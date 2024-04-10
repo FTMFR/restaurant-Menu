e4a268c75f8646fe24905390a0d6183ee66e50c3
FTMFR <fatemefarajzade018@gmail.com>
2024-04-10 15:34:56 +0330

    feat: implement ourMenu component
    Add OurMenu component for displaying menu items

--- src/Components/OurMenu.jsx
+++ src/Components/OurMenu.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Category from "./Category";
+import Posts from "./Posts";
+import menu from "../data";
 
-const OurMenu = () => {
+const OurMenu = ({ category }) => {
+  const [menu, setMenu] = useState(menu);
+  const [cate, setCate] = useState([]);
+
+  useEffect(() => {
+    const categor = menu.map((item) => item.category);
+    const setCates = new Set(categor);
+    setCate(Array.from[setCates]);
+    console.log(setCates);
+  }, []);
+
   return (
-    <div>OurMenu</div>
-  )
-}
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        {cate &&
+          cate.map((category) => (
+            <div className="btn-container">
+              <Category category={category} />
+            </div>
+          ))}
+        <Posts />
+      </section>
+    </main>
+  );
+};
 
-export default OurMenu
+export default OurMenu;
